33c87f8fdb89f1556c0d8b7023c0f9923a429ac0
matthias03 <maciej.kloda98@gmail.com>
2024-02-13 13:51:05 +0100

    Created React project with Type Script

---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,16 @@
+import "./Nav.css";
+
+function Nav() {
+    return (
+        <div className="nav--wrap">
+            <div className="menu--wrap">
+                <p>Mens</p>
+                <p>Womens</p>
+                <p>Kids</p>
+            </div>
+            <div></div>
+        </div>
+    );
+}
+
+export default Nav;
