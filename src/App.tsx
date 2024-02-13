dd8a1ae0ece1fa855fd9e45b27d0701cb5547aa8
matthias03 <maciej.kloda98@gmail.com>
2024-02-13 21:40:31 +0100

    Added file to handle image import, made home page template

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Nav from "./components/Nav/Nav";
+import HomePage from "./components/HomePage/HomePage";
 import './App.css';
 
 function App() {
   return (
     <div className="container">
-      <Nav></Nav>
+        <Nav />
+        <HomePage />
     </div>
   );
 }
---
+++ src/components/HomePage/HomePage.tsx
@@ -0,0 +1,28 @@
+import background from "../../assets/images/background.jpg";
+import "./HomePage.css";
+
+function HomePage() {
+    return(
+        <div className="home--page--container">
+            <div className="home--baner">
+                <img src={background} alt="Background"/>
+            </div>
+            <div className="new--wrap">
+                <p>Freshly added</p>
+                <div className="tiles--wrap">
+
+                </div>
+            </div>
+            <div className="coming--wrap">
+                <p>Coming soon</p>
+                <div></div>
+            </div>
+            <div className="bestsellers--wrap">
+                <p>Bestsellers</p>
+                <div></div>
+            </div>
+        </div>
+    );
+}
+
+export default HomePage;
