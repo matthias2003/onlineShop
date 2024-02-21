3d22f793505048036a02b1445688107d092029bb
matthias03 <maciej.kloda98@gmail.com>
2024-02-21 11:48:32 +0100

    Added Coming soon preview

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,8 +1,8 @@
 import background from "../../assets/images/background.jpg";
 import { fetchData } from "../../requests";
-import "./HomePage.css";
+import ComingSoonImage from "../../assets/images/Air-Jordan-4-Seafoam-AQ9129-103-04.jpg"
 import {useEffect, useState} from "react";
-
+import "./HomePage.css";
 function HomePage() {
     const [ sneakerData, setSneakerData ] = useState([]);
 
@@ -38,11 +38,27 @@ function HomePage() {
             </div>
             <div className="coming--wrap">
                 <p>Coming soon</p>
-                <div></div>
+                <div className="coming--content">
+                    <p>AIR JORDAN 4</p>
+                    <img src={ComingSoonImage} alt="Coming soon sneaker" />
+                    <p>SEA FOAM</p>
+                </div>
             </div>
             <div className="bestsellers--wrap">
                 <p>Bestsellers</p>
-                <div></div>
+                <div className="tiles--wrap">
+                    {sneakerData.map((element: any) => {
+                        return (
+                            <div className="tile">
+                                <div className="img--wrap">
+                                    <img src={element.img} alt="Sneaker preview"/>
+                                </div>
+                                <p>{element.name}</p>
+                                <p>{element.price}</p>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
         </div>
     );
