ab7c1d1d3a3aec29d30b67e87d9f2245e73074a8
Maciej Kłoda <maciej.kloda98@gmail.com>
2024-02-25 17:05:02 +0100

    Made animated search button (need code refactor and clean up)

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,7 +1,11 @@
 import * as icon from "../../assets/icons/navIcons";
 import "./Nav.css";
+import {useState} from "react";
 
 function Nav() {
+
+    const  [ searchValue , setSearchValue ] = useState("");
+
     return (
         <div className="nav--wrap">
                 <ul className="menu--wrap">
@@ -10,7 +14,12 @@ function Nav() {
                     <li>Kids</li>
                 </ul>
             <div className="icons--wrap">
-                <div><img src={icon.search} alt="Search button"/></div>
+
+                    <div className="search-box">
+                        <button className="btn-search"><img src={icon.search} alt="Search button"/></button>
+                        <input type="text" className="input-search" placeholder="Search"/>
+                    </div>
+
                 <div><img src={icon.heart} alt="Favourites button"/></div>
                 <div><img src={icon.avatar} alt="Avatar button"/></div>
             </div>
